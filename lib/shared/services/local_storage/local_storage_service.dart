0efdf14c62523f5064e1faf4da723b6a0b36d38f
Lucas <lucasfarias.bsb@hotmail.com>
2023-08-10 15:42:01 -0300

    config local storage functions

---
+++ lib/shared/services/local_storage/local_storage_service.dart
@@ -0,0 +1,43 @@
+import 'dart:developer';
+
+import 'package:flutter_secure_storage/flutter_secure_storage.dart' as Storage;
+
+import '../../errors/local_storage_exceptions.dart';
+
+class LocalStorageService {
+  Future<void> set(String key, String? data) async {
+    const storage = Storage.FlutterSecureStorage();
+
+    try {
+      await storage.write(key: key, value: data);
+    } catch (error, st) {
+      final errorMessage = "Error storing data: $data with key : $key";
+      log(errorMessage, error: error, stackTrace: st);
+      throw LocalStorageException(errorMessage);
+    }
+  }
+
+  Future<String?> get(String key) async {
+    const storage = Storage.FlutterSecureStorage();
+
+    try {
+      return await storage.read(key: key);
+    } catch (error, st) {
+      final errorMessage = "Error reading key: $key";
+      log(errorMessage, error: error, stackTrace: st);
+      throw LocalStorageException(errorMessage);
+    }
+  }
+
+  Future<void> clearStorage(String key) async {
+    const storage = Storage.FlutterSecureStorage();
+
+    try {
+      return await storage.deleteAll();
+    } catch (error, st) {
+      const errorMessage = "Error clearing storage";
+      log(errorMessage, error: error, stackTrace: st);
+      throw LocalStorageException(errorMessage);
+    }
+  }
+}
