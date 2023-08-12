5bdfcf2a35a9f00a42778be9ec46aa76e356a4f7
Lucas <lucasfarias.bsb@hotmail.com>
2023-08-12 17:49:17 -0300

    loadTodos controller

---
+++ lib/features/todos/controllers/todos_controller.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:todo_list_provider/shared/models/todo_model.dart';
+import 'package:todo_list_provider/shared/services/local_storage/todos_local_storage_service.dart';
+
+class TodosController extends ChangeNotifier {
+  final TodosLocalStorageService _todosLocalStorageService;
+
+  TodosController(this._todosLocalStorageService);
+
+  final List<TodoModel> todos = [];
+
+  final List<String> doneTodos = [];
+
+  Future<String?> loadTodos() async {
+    final (String? error, List<TodoModel>? loadedTodos) =
+        await _todosLocalStorageService.getTodos();
+
+    if (error == null) {
+      todos
+        ..clear()
+        ..addAll(loadedTodos!);
+
+      sortTodosByDate();
+    }
+
+    return error;
+  }
+
+  void sortTodosByDate() {
+    todos.sort((todoA, todoB) => todoA.date.compareTo(todoB.date));
+    notifyListeners();
+  }
+}
