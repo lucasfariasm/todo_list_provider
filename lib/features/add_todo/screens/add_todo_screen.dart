119450863acfd420f98da7362756d279b57528e2
Lucas <lucasfarias.bsb@hotmail.com>
2023-08-16 18:29:05 -0300

    add text input widget

--- lib/features/add_todo/screens/add_todo_screen.dart
+++ lib/features/add_todo/screens/add_todo_screen.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list_provider/shared/widgets/texts/text_widget.dart';
 
-class AddTodoScreen extends StatelessWidget {
+import '../../../shared/widgets/inputs/text_input_widget.dart';
+
+class AddTodoScreen extends StatefulWidget {
   const AddTodoScreen({super.key});
 
   @override
+  State<AddTodoScreen> createState() => _AddTodoScreenState();
+}
+
+class _AddTodoScreenState extends State<AddTodoScreen> {
+  final _formKey = GlobalKey<FormState>();
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const TextWidget('Criar Tarefa'),
+        iconTheme: const IconThemeData(color: Colors.white),
+      ),
+      body: SingleChildScrollView(
+          child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextInputWidget(
+                controller: TextEditingController(),
+                focusNode: FocusNode(),
+                label: 'Título',
+              ),
+            ],
+          ),
+        ),
+      )),
+    );
   }
 }
